1dba7f06323343f5e17029b4692b89b967706ee9
J.Javier Delgado <dcjosej@gmail.com>
2016-07-11 18:43:00 +0200

    API Gamejolt

---
+++ Assets/Scripts/ScoreTimeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class ScoreTimeController : MonoBehaviour
+{
+	public float timerCount;
+	private TimeSpan timeSpan = new TimeSpan(0);
+
+	public Text guiScoreTimer;
+
+	void Update()
+	{
+		timerCount += Time.deltaTime;
+		
+		float ss = Mathf.Floor(timerCount % 60);
+		float mm = Mathf.Floor(timerCount / 60);
+
+		Debug.Log("" + ss);
+
+		guiScoreTimer.text = string.Format("{0:00}:{1:00}", mm, ss);
+	}
+}
---
+++ Assets/Scripts/SocialManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SocialManager : MonoBehaviour
+{
+	void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+
+	void Start()
+	{
+		GameJolt.UI.Manager.Instance.ShowSignIn();
+	}
+
+
+	#region TESTING METHODS
+	public void AddScore()
+	{
+		int scoreValue = 150;
+		string scoreText = "150 seconds, Congrats!";
+		int tableID = 0;
+		string extraData = "";
+		GameJolt.API.Scores.Add(scoreValue, scoreText, 0, "", null);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.P))
+		{
+			AddScore();
+		}
+	}
+	#endregion
+}
