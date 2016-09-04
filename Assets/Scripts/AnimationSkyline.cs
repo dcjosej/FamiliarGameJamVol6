44ee5690fe33606f88dff0d58881a61d9d8551ae
J.Javier Delgado <dcjosej@gmail.com>
2016-09-04 13:07:20 +0200

    animacion skyline

---
+++ Assets/Scripts/AnimationSkyline.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSkyline : MonoBehaviour {
+
+	public float velocity;
+	private Vector3 velocityVector;
+	private Vector3 respawnPosition;
+
+	public Transform skyline1;
+	public Transform skyline2;
+
+	private Transform fontSkyline;
+
+	void Start()
+	{
+		velocityVector = new Vector3(0f, 0f, -velocity);
+		respawnPosition = new Vector3(0f, 0f, 330);
+		fontSkyline = skyline1;
+	}
+
+	void Update ()
+	{
+		skyline1.localPosition += velocityVector * Time.deltaTime;
+		skyline2.localPosition += velocityVector * Time.deltaTime;
+
+		if (fontSkyline.localPosition.z <= -886)
+		{
+			if(fontSkyline.gameObject.name == "Skyline1")
+			{
+				skyline1.localPosition = respawnPosition;
+				fontSkyline = skyline2;
+			}
+			else
+			{
+				skyline2.localPosition = respawnPosition;
+				fontSkyline = skyline1;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/AnimationSkyline2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSkyline2 : MonoBehaviour {
+
+	public float velocity;
+	private Vector3 velocityVector;
+	private Vector3 respawnPosition;
+
+
+	void Start()
+	{
+		velocityVector = new Vector3(0f, 0f, -velocity);
+		respawnPosition = new Vector3(0f, 0f, 609);
+	}
+
+	void Update ()
+	{
+		transform.localPosition += velocityVector * Time.deltaTime;
+
+		if(transform.localPosition.z <= -259)
+		{
+			transform.localPosition = respawnPosition;
+		}
+	}
+}
